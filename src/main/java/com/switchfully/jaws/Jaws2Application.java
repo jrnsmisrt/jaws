5850319d1de3968c9a7145caea0a986a9d1f1176
jrnsmisrt <jeroen.smissaert@gmail.com>
2021-12-20 12:46:57 +0100

    springboot setup files

---
+++ src/main/java/com/switchfully/jaws/Jaws2Application.java
@@ -0,0 +1,13 @@
+package com.switchfully.jaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaws2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jaws2Application.class, args);
+    }
+
+}
