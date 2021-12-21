1f82b13f18ca28446825ad450e75a797b07e51a3
enoirhom <emil.noirhomme@student.unamur.be>
2021-12-21 14:17:05 +0100

    Broken for review

--- src/main/java/com/switchfully/jaws/api/SecurityTestController.java
+++ src/main/java/com/switchfully/jaws/api/SecurityTestController.java
@@ -1,5 +1,9 @@
 package com.switchfully.jaws.api;
 
+import com.switchfully.jaws.services.security.KeycloakService;
+import com.switchfully.jaws.services.security.Role;
+import com.switchfully.jaws.services.security.dto.KeycloakUserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -8,12 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/test")
 public class SecurityTestController {
 
+    private final KeycloakService keycloakService;
+
+    @Autowired
+    public SecurityTestController(KeycloakService keycloakService) {
+        this.keycloakService = keycloakService;
+    }
 
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasAuthority('CREATE_PARKING_LOT')")
     void test() {
-        System.out.println("Had access");
+        KeycloakUserDTO user = new KeycloakUserDTO("Léonie", "Emil", Role.MANAGER);
+        keycloakService.addUser(user);
     }
 
 }
