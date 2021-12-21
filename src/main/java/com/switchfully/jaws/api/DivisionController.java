ed4cc85b119794f680fac94b2f973e5a64ec80ea
Maxim Verbeeck <maximverbeeck@hotmail.com>
2021-12-21 13:15:28 +0100

    testing repo - tryout

--- src/main/java/com/switchfully/jaws/api/DivisionController.java
+++ src/main/java/com/switchfully/jaws/api/DivisionController.java
@@ -6,6 +6,7 @@ import com.switchfully.jaws.services.division.dtos.DivisionDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class DivisionController {
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public DivisionDto createDivision(CreateDivisionDto createDivisionDTO) {
+    public DivisionDto createDivision(@RequestBody CreateDivisionDto createDivisionDTO) {
         return divisionService.createDivision(createDivisionDTO);
     }
 }
--- src/test/java/com/switchfully/jaws/repositories/DivisionRepositoryTest.java
+++ src/test/java/com/switchfully/jaws/repositories/DivisionRepositoryTest.java
@@ -1,31 +1,47 @@
 package com.switchfully.jaws.repositories;
 
+
 import com.switchfully.jaws.domain.Division;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+public class DivisionRepositoryTest {
+    private Division division;
+    private Division dummyDivision;
 
-//@SpringJUnitConfig(DatasourceConfig.class)
-class DivisionRepositoryTest {
-
-
+    @Autowired
     private DivisionRepository divisionRepository;
 
-    public DivisionRepositoryTest(DivisionRepository divisionRepository){
-        this.divisionRepository = divisionRepository;
+    @BeforeEach
+    public void setup() {
+        division = new Division.DivisionBuilder()
+                .withName("Maxim")
+                .withDirectorFullName("BossMan")
+                .withOriginalName("Number One")
+                .build();
+
+        dummyDivision = new Division.DivisionBuilder()
+                .withName("Maxim")
+                .withDirectorFullName("Lalalala")
+                .withOriginalName("Jack Sparrow")
+                .build();
     }
 
     @Test
-    void save() {
-
-        Assertions.assertThat(true).isFalse();
+    public void givenDivision_whenDivisionWithSameName_thenShouldBeEqual() {
+        Assertions.assertEquals(division.getName(), dummyDivision.getName());
     }
 
-
-
-
-
-
+    @Test
+    public void givenDivision_whenSavedInRepository_thenReturnSizeOne() {
+        divisionRepository.save(division);
+        int amount = divisionRepository.findAll().size();
+        Assertions.assertEquals(1, amount);
+    }
 }
