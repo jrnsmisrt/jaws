b6502089424c4078d0278087a24fc4de6f9121d0
enoirhom <emil.noirhomme@student.unamur.be>
2021-12-21 12:02:37 +0100

    Broken push for review

--- src/main/java/com/switchfully/jaws/JawsApplication.java
+++ src/main/java/com/switchfully/jaws/JawsApplication.java
@@ -1,13 +1,39 @@
 package com.switchfully.jaws;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.keycloak.OAuth2Constants;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 
 @SpringBootApplication
 public class JawsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JawsApplication.class, args);
+    }
+
+    /**
+     * This creates a client that allows you send requests to keycloak as an admin
+     * <p>
+     */
+    @Bean
+    public Keycloak keycloak(@Value("${master.keycloak.username}") String adminUsername, @Value("${master.keycloak.password}") String adminPassword) {
+        return KeycloakBuilder.builder()
+                .serverUrl("https://keycloak.switchfully.com/auth")
+                .grantType(OAuth2Constants.PASSWORD)
+                .realm("java-oct-2021")
+                .clientId("parkshark-jaws")
+                .username(adminUsername)
+                .password(adminPassword)
+                .resteasyClient(
+                        new ResteasyClientBuilder()
+                                .connectionPoolSize(10).build()
+                )
+                .build();
     }
 
 }
---
+++ src/main/java/com/switchfully/jaws/services/security/Role.java
@@ -0,0 +1,26 @@
+package com.switchfully.jaws.services.security;
+
+import java.util.List;
+
+import static com.switchfully.jaws.services.security.Feature.*;
+
+public enum Role {
+    MANAGER("manager", CREATE_PARKING_LOT, GET_DIVISION_OVERVIEW, CREATE_DIVISION, CREATE_SUBDIVISION, GET_PARKING_LOT_OVERVIEW, GET_PARKING_ALLOCATION_OVERVIEW),
+    MEMBER("member", SELECT_MEMBERSHIP, CREATE_ALLOCATION_PARKING_SPOT, STOP_ALLOCATION_PARKING_SPOT);
+
+    private final String label;
+    private final List<Feature> featureList;
+
+    Role(String label, Feature... featureList) {
+        this.label = label;
+        this.featureList = List.of(featureList);
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public List<Feature> getFeatures() {
+        return featureList;
+    }
+}
