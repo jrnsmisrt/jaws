8ba4840f34515393c5f95065a7a80ec47e2b7778
jrnsmisrt <jeroen.smissaert@gmail.com>
2021-12-21 13:37:46 +0100

    commit mock&stub UserServiceTest :: -AddingUserToDatabaseWillReturnCorrectUser -verifyUserRepositorySaveIsExecuted

--- src/test/java/com/switchfully/jaws/services/user/UserServiceTest.java
+++ src/test/java/com/switchfully/jaws/services/user/UserServiceTest.java
@@ -1,6 +1,10 @@
 package com.switchfully.jaws.services.user;
 
+import com.switchfully.jaws.domain.Address;
+import com.switchfully.jaws.domain.ContactInformation;
+import com.switchfully.jaws.domain.User;
 import com.switchfully.jaws.repositories.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -10,17 +14,49 @@ class UserServiceTest {
     private UserRepository userRepository;
     private UserService userService;
 
-    UserServiceTest(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final Address address = new Address.AddressBuilder()
+            .withCity("Gent")
+            .withCountry("Belgium")
+            .withStreet("husestraat")
+            .withStreetNumber("22")
+            .withZipCode(9000)
+            .build();
+
+    private final ContactInformation contactInformation = new ContactInformation.ContactInfoBuilder()
+            .withCellPhoneNumber("0458235")
+            .withEmailAddress("Jeroen.smissaert@outlook.com")
+            .withHomePhoneNumber("5405465")
+            .build();
+
+    private final User testUser = new User.UserBuilder()
+            .withAddress(address)
+            .withContactInformation(contactInformation)
+            .withFirstName("Jeroen")
+            .withLastName("Smissaert")
+            .withLicensePlate("B2020")
+            .build();
+
 
     @BeforeEach
     void setUpStockService() {
         userRepository = Mockito.mock(UserRepository.class);
+        userService = new UserService(userRepository);
+
     }
 
     @Test
-    void AddingUserToDatabaseWillReturnCorrectUser() {
+    void addingUserToDatabaseWillReturnCorrectUser() {
+        Mockito.when(userRepository.save(testUser)).thenReturn(testUser);
+        Assertions.assertThat(userService.addUser(testUser)).isEqualTo(testUser);
+
 
     }
+
+    @Test
+    void verifyUserRepositorySaveIsExecuted() {
+        userService.addUser(testUser);
+        Mockito.verify(userRepository).save(testUser);
+    }
+
+
 }
