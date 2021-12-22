332a9a0e83fff3c2e508075ac191d610b11d42f2
Ben Mees <benmees@outlook.com>
2021-12-22 09:36:22 +0100

    - Merge main into our branche

--- src/main/resources/db/migration/V2__alteruserstable.sql
+++
@@ -1,5 +0,0 @@
-Alter table users
- add column fk_address_id bigint;
-Alter table users
-drop column if exists fk_user;
-
--- src/main/resources/db/migration/V4__makeAForeignkeyUserAderss.sql
+++
@@ -1,2 +0,0 @@
-Alter table users
-   ADD CONSTRAINT fk_address_id FOREIGN KEY(fk_address_id) REFERENCES address(id_address)
