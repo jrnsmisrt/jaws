1ee0005cf5cd30bfb69421cffbcd5b39ae6a5102
enoirhom <emil.noirhomme@student.unamur.be>
2021-12-23 11:41:21 +0100

    Refactor

--- src/test/java/com/switchfully/jaws/api/ParkingLotControllerIntegrationTest.java
+++ src/test/java/com/switchfully/jaws/api/ParkingLotControllerIntegrationTest.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("integration test")
 class ParkingLotControllerIntegrationTest {
 
     @Autowired
